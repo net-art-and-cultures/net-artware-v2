0da7651b6961e0c7e343b2212943ca3b9c5e5769
Sherrie Fay Lin <linsherrie@gmail.com>
2021-04-27 17:42:20 -0500

    adding brush

---
+++ js/brushes/coffeestainbrush.js
@@ -0,0 +1,28 @@
+var t;
+var img;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  stroke(45,0,20, 0.8);
+  noFill();
+  t = 0;
+}
+
+function draw() {
+   // image(img,200,200, 210.5, 77.76);
+  translate(mouseX, mouseY);
+  beginShape();
+  for (var i = 0; i < 1000; i++) {
+    var ang = map(i, 0, 250, 0, random(0,10));
+    var rad = 200 * noise(i, t * 0.0005);
+    var x = rad * cos(ang);
+    var y = rad * sin(ang);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
+
+  t += 1;
+
+}
