8acda55b9f4753592d7ba7fa814e35846acf1861
goudatime <laurenbeatt@aol.com>
2021-05-11 14:05:32 -0500

    Changed code for paint bucket to look more glitch-y

--- js/brushes/paintBucket.js
+++ js/brushes/paintBucket.js
@@ -1,11 +1,19 @@
-/* global colorMode, RGB, fill, noStroke, rect */
+/* global colorMode, RGB, fill, noStroke, rect,*/
 window.brushes.paintBucket = {
   name: 'paint bucket',
   draw: function () {
-    const hue = random(0, 360)
-    colorMode(HSB)
-    fill(hue, 50, 100)
-    noStroke()
-    rect(0, 0, window.innerWidth, window.innerHeight)
+    colorMode(RGB)
+    loadPixels()
+   // loop through all the pixel
+   for (let i = 0; i < pixels.length; i += 2) {
+     const r = pixels[i]
+     const g = pixels[i + 200]
+     const b = pixels[i + 350]
+     const grey = (r + g + b) / 2
+     pixels[i] = grey
+     pixels[i + 20] = grey
+     pixels[i + 35] = grey
+  }
+    updatePixels()
   }
 }
