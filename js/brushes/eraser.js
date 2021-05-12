52777c4eb7c5159b4bdaf5a46f7a65ed72f69292
jtao7 <jtao7@artic.edu>
2021-05-12 22:31:38 +0900

    changed the brush opacity

--- js/brushes/eraser.js
+++ js/brushes/eraser.js
@@ -5,7 +5,7 @@ window.brushes.eraser = {
   draw: function () {
     slider= document.getElementById("eraserSlider")
     colorMode(RGB)
-    fill(255, 255, 255, 255)
+    fill(255, 255, 255)
     noStroke()
     const radius = slider.value
     ellipse(window.input.x, window.input.y, radius, radius)
