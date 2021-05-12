a02c0ae18cf6cee5473d88376aa0f9bbbdb7d4ef
jtao7 <jtao7@artic.edu>
2021-05-12 18:14:52 +0900

    changed eraser

--- js/brushes/eraser.js
+++ js/brushes/eraser.js
@@ -3,7 +3,7 @@ window.brushes.eraser = {
   name: 'eraser',
   draw: function () {
     colorMode(RGB)
-    fill(255, 255, 255)
+    fill(255, 255, 255,100)
     noStroke()
     const radius = 80
     ellipse(window.input.x, window.input.y, radius, radius)
